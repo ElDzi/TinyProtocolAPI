13fdf3ed8f72cea3f6366b67d8606c952d9639c6
ElDzi <jaroaga@onet.pl>
2016-05-19 12:18:16 +0200

    Update ActionBarAPI.java

--- com/gmail/zahusek/tinyprotocolapi/api/actionbar/ActionBarAPI.java
+++ com/gmail/zahusek/tinyprotocolapi/api/actionbar/ActionBarAPI.java
@@ -1,5 +1,17 @@
 package com.gmail.zahusek.tinyprotocolapi.api.actionbar;
 
+import com.gmail.zahusek.tinyprotocolapi.TinyProtocol;
+import com.gmail.zahusek.tinyprotocolapi.TinyProtocolAPI;
+import com.gmail.zahusek.tinyprotocolapi.packet.PacketChat;
+
+import static com.gmail.zahusek.tinyprotocolapi.wrapper.WrapperEnum.TitleAction.*;
+
 public abstract class ActionBarAPI {
-static final TinyProtocol fb = TinyProtocolAPI.getTinyProtocol();
+  static final TinyProtocol fb = TinyProtocolAPI.getTinyProtocol();
+
+	public static void hotbar(Player player, String msg)
+	{     	
+		if(player == null) throw new IllegalArgumentException("Player cannot be null !");
+		new PacketChat(msg, (byte) 2); 
+	}
 }
